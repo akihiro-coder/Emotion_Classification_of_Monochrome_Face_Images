d4158dced90eb87e0a28f0afcc834b8df89acb16
akihiro <ScuderiaFerrariSebastian@gmail.com>
2022-11-20 02:34:08 +0900

    define model structure, create object of model and define criterion

--- train_torch.py
+++ train_torch.py
@@ -48,16 +48,36 @@ class CNN(nn.Module):
 
         # 特徴抽出層
         self.features = nn.Sequential(
-                nn.Conv2D(in_channels=1, out_channels=64, kernel_size=5, padding=2),
-                nn.ReLU(inplace=True),
-                nn.MaxPooling2D(kernel_size=2),
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
 
-                )
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
 
-        
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.classifier = nn.Linear(in_features=8 * 8 * 128, out_features=num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+# creating an object for CNN class
+model = CNN(num_classes=4)
+model.to(device) # on GPU
 
 # define criterion
-
+criterion = nn.CrossEntropyLoss()
 
 # define optimizer
 
